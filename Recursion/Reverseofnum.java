3da6beba3d59c13186e332f8e5593205905d2cc9
hitakshigabani21 <hitakshigabani@gmail.com>
2026-02-07 12:12:21 +0530

    recursion approach-1

--- Recursion/Reverseofnum.java
+++ Recursion/Reverseofnum.java
@@ -1,13 +1,24 @@
-// import java.util.*;
-// public class Reverseofnum {
-//     public static int findReverse(int num){
+import java.util.*;
+public class Reverseofnum {
+    public static int rev(int n, int m){
+        if(m==0 || n%10 == n){
+            return n;
+        }
+        return (n%10)*(int)(Math.pow(10, m))+rev(n/10, m-1);
         
-//     }
-//     public static void main(String args[]){
-//         Scanner sc = new Scanner(System.in);
-//         System.out.println("Enter a number to reverse:");
-//         int num = sc.nextInt();
-//         int rev = findReverse(num);
-//         System.out.println("Reversed number is: " + rev);
-//     }
-// }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int num = sc.nextInt();
+        int n = num;
+        int digits = 0;
+        while(n!=0){
+            n= n/10;
+            digits++;
+        }
+        
+        int rev = rev(num, digits-1);
+        System.out.println(rev);
+    }
+}
