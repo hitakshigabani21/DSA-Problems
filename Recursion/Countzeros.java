d36c47cbb9f9cd4b5d71ee83f98da0fb3363d9de
hitakshigabani21 <hitakshigabani@gmail.com>
2026-02-07 12:44:03 +0530

    count 0's by passing count in argument

--- Recursion/Countzeros.java
+++ Recursion/Countzeros.java
@@ -12,11 +12,26 @@ public class Countzeros {
         }
         countZero(n/10);
     }
+
+    public static int countZero2(int n, int count){
+        if(n == 0){
+            return count;
+        }
+
+        if(n%10 == 0){
+            return countZero2(n/10, count+1);
+        }else{
+            return countZero2(n/10, count);
+        }
+    }
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a number");
         int num = sc.nextInt();
         countZero(num);
         System.out.println(count);
+
+        int result = countZero2(num, 0);
+        System.out.println(result);
     }
 }
