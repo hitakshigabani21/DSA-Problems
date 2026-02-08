76d4009864484551cf81210f0e0eb5e7eeff3f3f
hitakshigabani21 <hitakshigabani@gmail.com>
2026-02-08 09:08:41 +0530

    final version

--- RecursiononArrays/Sortcheck.java
+++ RecursiononArrays/Sortcheck.java
@@ -5,10 +5,7 @@ public class Sortcheck {
         if(i==arr.length-1){
             return true;
         }
-        if(arr[i] < arr[i+1]){
-            return isSorted(arr, i+1);
-        }
-        return false;
+        return arr[i] < arr[i+1] && isSorted(arr, i+1);
     }
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
