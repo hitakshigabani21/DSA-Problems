3c7a0974f812ff427f92f16bd31fd0f0fd1d78ca
hitakshigabani21 <hitakshigabani@gmail.com>
2026-02-08 08:46:07 +0530

    recusrsion on arrays: 1. check whether array is sorted/not

---
+++ RecursiononArrays/Sortcheck.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class Sortcheck {
+    static boolean isSorted(int arr[], int i, int n){
+        if(i== n-1){
+            return arr[i] < arr[i+1];
+        }
+
+        return arr[i] < arr[i+1] && isSorted(arr, i+1, n);
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of the array: ");
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        System.out.println("Enter the elements of the array: ");
+        for(int i=0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        if(isSorted(arr, 0, n-1)){
+            System.out.println("The array is sorted in ascending order.");
+        } else {
+            System.out.println("The array is not sorted in ascending order.");
+        }
+
+    }
+}
