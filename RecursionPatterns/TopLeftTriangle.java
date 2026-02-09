23d03d30c7ff0fd15d14971f9840148d0cde221a
hitakshigabani21 <hitakshigabani@gmail.com>
2026-02-09 18:51:32 +0530

    No need of n parameter only row, col needed, in iterative too there were 2 loops , 2 vars

--- RecursionPatterns/TopLeftTriangle.java
+++ RecursionPatterns/TopLeftTriangle.java
@@ -1,23 +1,23 @@
 import java.util.*;
 
 public class TopLeftTriangle {
-    static void printTriangle(int n, int row, int col){
-        if(row == n){
+    static void printTriangle(int row, int col){
+        if(row == 0){
             return;
         }
-        if(col == n-row){
+        if(col == row){
            System.out.println();
-           printTriangle(n, row+1, 0);
+           printTriangle(row-1, 0);
            return;
         }
         System.out.print("* ");
-        printTriangle(n, row, col+1);
+        printTriangle(row, col+1);
     }
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number of rows: ");
         int n = sc.nextInt();
-        printTriangle(n, 0, 0);
+        printTriangle(n, 0);
 
     }
 }
