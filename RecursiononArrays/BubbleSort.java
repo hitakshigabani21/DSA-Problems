74044127cf9310ada100a3efcbbc3ed8f19091b1
hitakshigabani21 <hitakshigabani@gmail.com>
2026-02-10 23:01:41 +0530

    made some changes

--- RecursiononArrays/BubbleSort.java
+++ RecursiononArrays/BubbleSort.java
@@ -1,21 +1,21 @@
 import java.util.*;
 public class BubbleSort {
     static void bubbleSort(int arr[], int i, int j){
-        if(j == arr.length-1){
-            return;
-        }
-
         if(i == arr.length-1){
-            bubbleSort(arr, 0, j+1);
             return;
         }
 
-        if(arr[i] > arr[i+1]){
-            int temp = arr[i];
-            arr[i] = arr[i+1];
-            arr[i+1] = temp;
+        if(j == arr.length-1-i){
+            bubbleSort(arr, i+1, 0);
+            return;
         }
-        bubbleSort(arr, i+1, j);
+
+        if(arr[j] > arr[j+1]){
+            int temp = arr[j];
+            arr[j] = arr[j+1];
+            arr[j+1] = temp;
+        }
+        bubbleSort(arr, i, j+1);
     }
     public static void main(String args[]){
         int arr[]={5,4,3,2,1};
