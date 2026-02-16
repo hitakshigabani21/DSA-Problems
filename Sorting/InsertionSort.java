ca07a0e6466a4d0d8d649abef652d34c99b9141a
hitakshigabani21 <hitakshigabani@gmail.com>
2026-02-16 19:29:14 +0530

    insertion sort with swapping correct approach

--- Sorting/InsertionSort.java
+++ Sorting/InsertionSort.java
@@ -3,16 +3,15 @@ import java.util.*;
 public class InsertionSort{
     static void sort(int arr[]){
         for(int i=0; i< arr.length-1; i++){
-            int j;
-            int temp = arr[i+1];
-            for(j=i; j>=0; j--){
-                if(temp < arr[j]){
-                    arr[j+1] = arr[j];
+            for(int j=i+1; j>0; j--){
+                if(arr[j] < arr[j-1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = temp;
                 }else{
-                    break;    
+                    break;
                 }
             }
-            arr[j+1] = temp;
         }
     }
     public static void main(String args[]){
